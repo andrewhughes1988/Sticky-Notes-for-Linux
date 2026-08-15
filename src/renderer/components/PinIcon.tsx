5ef6a998f210aef304742f898274f708eaa7ca1c
Andrew Hughes <andrewhughes1988@gmail.com>
2026-08-15 15:34:23 -0500

    style(ui): Update PinIcon to match provided screenshot silhouette leaning to the right

--- src/renderer/components/PinIcon.tsx
+++ src/renderer/components/PinIcon.tsx
@@ -20,20 +20,21 @@ export const PinIcon: React.FC<PinIconProps> = ({
       viewBox="0 0 24 24"
       fill={isPinned ? 'currentColor' : 'none'}
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.8"
       strokeLinecap="round"
       strokeLinejoin="round"
       className={className}
       style={{
         display: 'inline-block',
         verticalAlign: 'middle',
+        transform: 'rotate(35deg)',
         ...style,
       }}
     >
-      {/* Scaled-up Pushpin Body */}
-      <path d="M14.5 2.5l7 7-2.5 2.5-1-1-4.5 4.5 1.5 1.5-2 2-7.5-7.5 2-2 1.5 1.5 4.5-4.5-1-1L14.5 2.5z" />
-      {/* Needle Attached Directly to Collar Base */}
-      <line x1="9" y1="15" x2="2.5" y2="21.5" stroke="currentColor" fill="none" strokeWidth="2.2" />
+      {/* Pushpin Head & Flared Skirt (matches Screenshot 1 & 2, flipped along Y axis) */}
+      <path d="M7 3h10a1.5 1.5 0 0 1 1.5 1.5v.8a1.5 1.5 0 0 1-.8 1.4L16.2 7l-.8 5.2c1.8.8 3.6 1.8 3.6 3.6 0 .8-.6 1.2-1.5 1.2-1.5 0-3.5-.5-5.5-.5s-4 .5-5.5.5c-.9 0-1.5-.4-1.5-1.2 0-1.8 1.8-2.8 3.6-3.6L7.8 7l-1.5-.3a1.5 1.5 0 0 1-.8-1.4v-.8A1.5 1.5 0 0 1 7 3z" />
+      {/* Attached Needle Point */}
+      <path d="M12 17v6.5" strokeWidth="2.2" fill="none" />
     </svg>
   );
 };
